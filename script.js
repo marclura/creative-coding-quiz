a446b7161206d12426e600f27edd730ee8ea6365
marclura <29801332+marclura@users.noreply.github.com>
2024-11-04 21:53:00 +0100

    setup new version

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
+// buttons
+const buttonAsk = document.getElementById("button-ask");
+const buttonHelper = document.getElementById("button-help");
+const buttonChange = document.getElementById("button-change");
+
+
 const codesList = document.getElementsByTagName("code");
-const buttonQuizNext = document.getElementById("button-quiz-next");
-const buttonHelper = document.getElementById("button-helper");
-const buttonNextBlock = document.getElementById("button-next-block");
 const sectionHelpers = document.getElementById("helpers");
 let currentCodeBlock = 0;
 
 let spanList = codesList[currentCodeBlock].getElementsByTagName("span");
 
-
-buttonQuizNext.addEventListener('click', ()=> {
+// ask
+buttonAsk.addEventListener('click', ()=> {
     const spanArray = Array.from(spanList);
+    const currentSpanElement = codesList[currentCodeBlock].getElementsByClassName('selected')[0];
+
     spanArray.forEach((element) => {
         element.classList.remove('selected');
     })
-    const spanActive = spanList[Math.floor(Math.random()*spanList.length)];
-    console.log(spanActive.className);
+    let spanActive;
+
+    do {
+        spanActive = spanList[Math.floor(Math.random()*spanList.length)];
+    }
+    while(spanActive == currentSpanElement);    // avoid picking the same element twice
+
+    console.log(spanActive.dataset.type);
     spanActive.classList.add('selected');
 })
 
+// help
 buttonHelper.addEventListener('click', ()=> {
     sectionHelpers.classList.toggle("hidden");
 })
 
-buttonNextBlock.addEventListener('click', ()=> {
+// change
+buttonChange.addEventListener('click', ()=> {
     codesList[currentCodeBlock].classList.remove("active");
     if(currentCodeBlock < codesList.length - 1) {
         currentCodeBlock++;
