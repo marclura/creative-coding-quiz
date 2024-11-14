50467ad715cb2deea2238be41dc294e2178392e7
marclura <29801332+marclura@users.noreply.github.com>
2024-11-14 16:24:19 +0100

    better UI and functionalities

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const buttonAsk = document.getElementById("button-ask");
 const buttonHelper = document.getElementById("button-help");
 const buttonChange = document.getElementById("button-change");
+const buttonAnswer = document.getElementById("button-answer");
 
 
 const codesList = document.getElementsByTagName("code");
@@ -12,6 +13,15 @@ let spanList = codesList[currentCodeBlock].getElementsByTagName("span");
 
 let currentDataLanguage = "";
 
+
+function loadQuiz() {
+    
+    loadAnswerList();
+    
+    changeCode();
+}
+
+// change code
 function changeCode() {
     codesList[currentCodeBlock].classList.remove("active");
     if(currentCodeBlock < codesList.length - 1) {
@@ -22,13 +32,21 @@ function changeCode() {
     }
     codesList[currentCodeBlock].classList.add("active");
     spanList = codesList[currentCodeBlock].getElementsByTagName("span");
+
+    displayAnswer(false);
+    askNewQuestion();
 }
 
-changeCode();
+loadQuiz();
 
 
 // ask
 buttonAsk.addEventListener('click', ()=> {
+    displayAnswer(false);
+    askNewQuestion();
+})
+
+function askNewQuestion() {
     const spanArray = Array.from(spanList);
     const currentSpanElement = codesList[currentCodeBlock].getElementsByClassName('selected')[0];
 
@@ -43,10 +61,43 @@ buttonAsk.addEventListener('click', ()=> {
     }
     while(spanActive == currentSpanElement);    // avoid picking the same element twice
 
+    document.getElementById("correct-answer").innerText = spanActive.dataset.type;
+
     console.log(spanActive.dataset.type);
 
     spanActive.classList.add('selected');
-})
+}
+
+// load the answer list
+function loadAnswerList() {
+
+    let answerList = document.getElementById('answer-list');
+    const codeQuizSpans = document.getElementById("code-quiz").getElementsByTagName("span");
+    let dataTypeSpan = [];
+
+    for(span of codeQuizSpans) {
+        //console.log(span);
+        //console.log(span.dataset.type);
+        if(!dataTypeSpan.includes(span.dataset.type)) {
+            dataTypeSpan.push(span.dataset.type);
+            let li = document.createElement("li");
+            li.innerText = span.dataset.type;
+            answerList.appendChild(li);
+        }
+    }
+
+    console.log(dataTypeSpan);
+}
+
+function displayAnswer(active) {
+    const answer = document.getElementById("correct-answer-container");
+    if(active) {
+        answer.classList.remove("visually-hidden");
+    } else {
+        answer.classList.add("visually-hidden");
+    }
+    
+}
 
 // help
 buttonHelper.addEventListener('click', ()=> {
@@ -56,5 +107,10 @@ buttonHelper.addEventListener('click', ()=> {
 // change
 buttonChange.addEventListener('click', ()=> {
     changeCode();
+})
+
+// answer
+buttonAnswer.addEventListener('click', ()=> {
+    displayAnswer(true);
 })
 
